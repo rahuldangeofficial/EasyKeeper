6266f6905feba550bbdc3c38502336143a3835dd
Rahul Dange <rahuldangeofficial@gmail.com>
2023-07-12 04:40:28 +0530

    Add vercel .env format

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,13 @@ import "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_REACT_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_REACT_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_REACT_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_REACT_FIREBASE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_REACT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_REACT_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_REACT_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.VITE_REACT_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_REACT_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_REACT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_REACT_FIREBASE_BUCKET,
+  messagingSenderId: process.env.VITE_REACT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_REACT_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_REACT_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
