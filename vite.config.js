f96f9c5156f070673181e4a769aa29631340c7d1
Rahul Dange <rahuldangeofficial@gmail.com>
2023-07-12 04:49:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  __APP_ENV__: process.env.VITE_VERCEL_ENV,
   envDir: '.',
 })
